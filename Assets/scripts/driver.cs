69545908ca326c89b75cbf8ce6aec282b64bf6ab
vamsi <vammu920@gmail.com>
2020-03-02 22:38:37 +0530

    final build

---
+++ Assets/scripts/driver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class driver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    const float MoveUnitsPerSecond = 50;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput  = Input.GetAxis("Vertical");
+        Vector3 pos = transform.position;
+        if(horizontalInput != 0 ){
+            pos.x += horizontalInput * MoveUnitsPerSecond * Time.deltaTime;
+        }
+        if(verticalInput!=0){
+            pos.y += verticalInput * MoveUnitsPerSecond * Time.deltaTime;
+        }
+        transform.position = pos;
+    }
+}
